2d3f7aa21399e19ab5a454a444697f4b1821da58
Owen Kirby <oskirby@gmail.com>
2020-05-05 13:59:47 -0700

    Remove serial endpoints, we don't need them for DFU.
    USB-DFU only uses the control endpoint for doing both device enumeration
    and the transfer of firmware data.

--- usb/usb_uart.v
+++ usb/usb_uart.v
@@ -176,24 +176,6 @@ module usb_uart (
   wire ctrl_in_ep_stall;
   wire ctrl_in_ep_acked;
 
-
-  wire serial_out_ep_req;
-  wire serial_out_ep_grant;
-  wire serial_out_ep_data_avail;
-  wire serial_out_ep_setup;
-  wire serial_out_ep_data_get;
-  wire serial_out_ep_stall;
-  wire serial_out_ep_acked;
-
-  wire serial_in_ep_req;
-  wire serial_in_ep_grant;
-  wire serial_in_ep_data_free;
-  wire serial_in_ep_data_put;
-  wire [7:0] serial_in_ep_data;
-  wire serial_in_ep_data_done;
-  wire serial_in_ep_stall;
-  wire serial_in_ep_acked;
-
   wire sof_valid;
   wire [10:0] frame_index;
 
@@ -227,50 +209,13 @@ module usb_uart (
     .in_ep_acked(ctrl_in_ep_acked)
   );
 
-  usb_uart_bridge_ep usb_uart_bridge_ep_inst (
-    .clk(clk_48mhz),
-    .reset(reset),
-
-    // out endpoint interface
-    .out_ep_req(serial_out_ep_req),
-    .out_ep_grant(serial_out_ep_grant),
-    .out_ep_data_avail(serial_out_ep_data_avail),
-    .out_ep_setup(serial_out_ep_setup),
-    .out_ep_data_get(serial_out_ep_data_get),
-    .out_ep_data(out_ep_data),
-    .out_ep_stall(serial_out_ep_stall),
-    .out_ep_acked(serial_out_ep_acked),
-
-    // in endpoint interface
-    .in_ep_req(serial_in_ep_req),
-    .in_ep_grant(serial_in_ep_grant),
-    .in_ep_data_free(serial_in_ep_data_free),
-    .in_ep_data_put(serial_in_ep_data_put),
-    .in_ep_data(serial_in_ep_data),
-    .in_ep_data_done(serial_in_ep_data_done),
-    .in_ep_stall(serial_in_ep_stall),
-    .in_ep_acked(serial_in_ep_acked),
-
-    // uart pipeline in
-    .uart_in_data( uart_in_data ),
-    .uart_in_valid( uart_in_valid ),
-    .uart_in_ready( uart_in_ready ),
-
-    // uart pipeline out
-    .uart_out_data( uart_out_data ),
-    .uart_out_valid( uart_out_valid ),
-    .uart_out_ready( uart_out_ready ),
-
-    .debug(debug[3:0])
-  );
-
   wire nak_in_ep_grant;
   wire nak_in_ep_data_free;
   wire nak_in_ep_acked;
 
   usb_fs_pe #(
-    .NUM_OUT_EPS(5'd2),
-    .NUM_IN_EPS(5'd3)
+    .NUM_OUT_EPS(5'd1),
+    .NUM_IN_EPS(5'd1)
   ) usb_fs_pe_inst (
     .clk(clk_48mhz),
     .reset(reset),
@@ -284,24 +229,24 @@ module usb_uart (
     .dev_addr(dev_addr),
 
     // out endpoint interfaces
-    .out_ep_req({serial_out_ep_req, ctrl_out_ep_req}),
-    .out_ep_grant({serial_out_ep_grant, ctrl_out_ep_grant}),
-    .out_ep_data_avail({serial_out_ep_data_avail, ctrl_out_ep_data_avail}),
-    .out_ep_setup({serial_out_ep_setup, ctrl_out_ep_setup}),
-    .out_ep_data_get({serial_out_ep_data_get, ctrl_out_ep_data_get}),
+    .out_ep_req({ctrl_out_ep_req}),
+    .out_ep_grant({ctrl_out_ep_grant}),
+    .out_ep_data_avail({ctrl_out_ep_data_avail}),
+    .out_ep_setup({ctrl_out_ep_setup}),
+    .out_ep_data_get({ctrl_out_ep_data_get}),
     .out_ep_data(out_ep_data),
-    .out_ep_stall({serial_out_ep_stall, ctrl_out_ep_stall}),
-    .out_ep_acked({serial_out_ep_acked, ctrl_out_ep_acked}),
+    .out_ep_stall({ctrl_out_ep_stall}),
+    .out_ep_acked({ctrl_out_ep_acked}),
 
     // in endpoint interfaces
-    .in_ep_req({1'b0, serial_in_ep_req, ctrl_in_ep_req}),
-    .in_ep_grant({nak_in_ep_grant, serial_in_ep_grant, ctrl_in_ep_grant}),
-    .in_ep_data_free({nak_in_ep_data_free, serial_in_ep_data_free, ctrl_in_ep_data_free}),
-    .in_ep_data_put({1'b0, serial_in_ep_data_put, ctrl_in_ep_data_put}),
-    .in_ep_data({8'b0, serial_in_ep_data[7:0], ctrl_in_ep_data[7:0]}),
-    .in_ep_data_done({1'b0, serial_in_ep_data_done, ctrl_in_ep_data_done}),
-    .in_ep_stall({1'b0, serial_in_ep_stall, ctrl_in_ep_stall}),
-    .in_ep_acked({nak_in_ep_acked, serial_in_ep_acked, ctrl_in_ep_acked}),
+    .in_ep_req({ctrl_in_ep_req}),
+    .in_ep_grant({ctrl_in_ep_grant}),
+    .in_ep_data_free({ctrl_in_ep_data_free}),
+    .in_ep_data_put({ctrl_in_ep_data_put}),
+    .in_ep_data({ctrl_in_ep_data[7:0]}),
+    .in_ep_data_done({ctrl_in_ep_data_done}),
+    .in_ep_stall({ctrl_in_ep_stall}),
+    .in_ep_acked({ctrl_in_ep_acked}),
 
     // sof interface
     .sof_valid(sof_valid),
